8ccac618b4516c4e3123642063765290fa755a1b
carl alberto <cralberto11@gmail.com>
2017-01-26 23:04:02 +0800

    theme anyone?

---
+++ themes/drone-aerial-photographer/inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Drone Aerial Photographer Theme Customizer
+ *
+ * @package Drone_Aerial_Photographer
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function drone_aerial_photographer_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'drone_aerial_photographer_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function drone_aerial_photographer_customize_preview_js() {
+	wp_enqueue_script( 'drone_aerial_photographer_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'drone_aerial_photographer_customize_preview_js' );
